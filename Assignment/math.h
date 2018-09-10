7bbdeaff6d20bb775dfe98f36c51c6560b82f7fe
ccraig427 <35497783+ccraig427@users.noreply.github.com>
2018-09-10 16:13:37 -0400

    Update math.h

--- Assignment/math.h
+++ Assignment/math.h
@@ -26,4 +26,8 @@
 //Part of your documentation should be listing the valid inputs and outputs for the functions you create.
 int math(int num1, int num2, char Operator);
 
+int num1; 
+int num2; 
+char Operator; 
+
 #endif /* MATH_H_ */
